eb6f02923ab6edb7a597a2f31cf902cf2fbe4961
Daan Kuik <dkuik@sogyo.nl>
2023-08-03 15:22:58 +0200

    Complete GuessWord class with output and checking

--- src/main/java/nl/sogyo/javaopdrachten/GuessWord.java
+++ src/main/java/nl/sogyo/javaopdrachten/GuessWord.java
@@ -1,22 +1,83 @@
 package nl.sogyo.javaopdrachten;
 
+import java.util.ArrayList;
+
 public class GuessWord {
     
-    private String wordToGuess;
-    
+    private char[] wordToGuess;
+    private char[] correctCharacters;
+    private ArrayList<Character> incorrectCharacters;
+
+
     public GuessWord() {
         setWordToGuess();
+        setGuessedLetters();
     }
 
-    public void setWordToGuess() {
-        wordToGuess = "HANGMAN";
+
+    private void setWordToGuess() {
+        String guessWord = getWordToGuess();
+        wordToGuess = guessWord.toCharArray();
     }
 
-    public void printWordToGuess() {
-        System.out.println(wordToGuess);
+
+    private String getWordToGuess() { 
+        return "HANGMAN"; //default for now, may randomize?
     }
 
-    // private static char[] getUniqueLetters(){
+
+    private void setGuessedLetters() {
+        correctCharacters = new char[wordToGuess.length];
+        incorrectCharacters = new ArrayList<Character>();
+    }
+
+
+    public void printCorrectCharacters() {
+        System.out.println("Current known word: ");
+        for(int i = 0; i < correctCharacters.length; i++) {
+            if(isNotSet(correctCharacters[i])) {
+                System.out.print("_");
+            } else {
+                System.out.print(correctCharacters[i]);
+            }
+        }
+        System.out.println();
+    }
+
+
+    private boolean isNotSet(char c) {
+        return (c == '\u0000');
+    }
+
+
+    public void printIncorrectCharacters() {
+        System.out.println("Incorrect guesses: ");
+        for(int i = 0; i < incorrectCharacters.size(); i++) {
+            System.out.print(incorrectCharacters.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+
+    public boolean checkGuess(char guessedLetter) {
+        boolean isCorrect = false;
         
-    // }
+        for(int i = 0; i < wordToGuess.length; i++) {
+            if(wordToGuess[i] == guessedLetter) {
+                isCorrect = true;
+                setCorrectLetter(guessedLetter, i);
+            }
+        }
+
+        if(!isCorrect) {
+            incorrectCharacters.add(guessedLetter);
+        }
+
+        return isCorrect;
+    }
+
+
+    private void setCorrectLetter(char guessedLetter, int position) {
+        correctCharacters[position] = guessedLetter;
+    }
 }
