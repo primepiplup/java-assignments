ff661893ba73f45f13c2db62033cacf40236392e
Daan Kuik <dkuik@sogyo.nl>
2023-08-04 16:02:23 +0200

    Added some robot commands to the main function

--- src/main/java/nl/sogyo/javaopdrachten/RoboRally.java
+++ src/main/java/nl/sogyo/javaopdrachten/RoboRally.java
@@ -4,9 +4,12 @@ public class RoboRally {
     public static void main(String[] args) {
         Robot robotOne = new Robot();
         robotOne.printState();
+        robotOne.forward();
         robotOne.turnLeft();
         robotOne.printState();
         robotOne.forward();
+        robotOne.printState();
+        robotOne.backward();
         robotOne.printState();
     }
 }
