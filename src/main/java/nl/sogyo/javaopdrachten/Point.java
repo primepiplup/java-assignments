70b8f6650fab87ee53a43365567d8d8598ff7541
Daan Kuik <dkuik@sogyo.nl>
2023-08-04 14:23:26 +0200

    Add move in direction functionality to Point class

--- src/main/java/nl/sogyo/javaopdrachten/Point.java
+++ src/main/java/nl/sogyo/javaopdrachten/Point.java
@@ -12,4 +12,50 @@ public class Point {
     public void print() {
         System.out.printf("(%d, %d)", x, y);
     }
+
+    public void moveDistanceInDirection(int distance, Direction direction) {
+        switch(direction) {
+            case NORTH:
+                moveNorth(distance);
+                break;
+            case EAST:
+                moveEast(distance);
+                break;
+            case SOUTH:
+                moveSouth(distance);
+                break;
+            case WEST:
+                moveWest(distance);
+                break;
+        }
+    }
+
+    public void moveDistanceInDirection(Direction direction) {
+        int defaultMoveDistance = 1;
+        moveDistanceInDirection(defaultMoveDistance, direction);
+    }
+
+    public void moveEast(int distance) {
+        moveOnX(distance);
+    }
+
+    public void moveWest(int distance) {
+        moveOnX(-distance);
+    }
+
+    public void moveNorth(int distance) {
+        moveOnY(distance);
+    }
+
+    public void moveSouth(int distance) {
+        moveOnY(-distance);
+    }
+
+    private void moveOnX(int deltaX) {
+        x += deltaX;
+    }
+
+    private void moveOnY(int deltaY) {
+        y += deltaY;
+    }
 }
