a03b04cf2366e775ec81ea9f4b7e58ed3328fa78
Daan Kuik <dkuik@sogyo.nl>
2023-08-04 17:18:57 +0200

    Created Fraction class and added toString method

---
+++ src/main/java/nl/sogyo/javaopdrachten/Fraction.java
@@ -0,0 +1,17 @@
+package nl.sogyo.javaopdrachten;
+
+public class Fraction {
+    int numerator;
+    int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public String toString() {
+        String numeratorString = String.valueOf(numerator);
+        String denominatorString = String.valueOf(denominator);
+        return numeratorString + "/" + denominatorString;
+    }
+}
