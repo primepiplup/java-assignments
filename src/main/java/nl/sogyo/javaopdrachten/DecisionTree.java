a218644ad3ade670a32641b280455252cbff55ba
Daan Kuik <dkuik@sogyo.nl>
2023-08-07 13:48:10 +0200

    Remove unnecessary imports

--- src/main/java/nl/sogyo/javaopdrachten/DecisionTree.java
+++ src/main/java/nl/sogyo/javaopdrachten/DecisionTree.java
@@ -1,9 +1,5 @@
 package nl.sogyo.javaopdrachten;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class DecisionTree {
     public static void main(String[] args) {
         Tree tree = new Tree("/home/daan/Projects/java-opdrachten-daan/src/main/java/nl/sogyo/javaopdrachten/decision-tree-data.txt");
