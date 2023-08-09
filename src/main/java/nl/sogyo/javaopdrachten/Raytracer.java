13680f3bed7ae6d2c3f9433afe68974c936976ee
Daan Kuik <dkuik@sogyo.nl>
2023-08-09 10:54:31 +0200

    Added arraylists to hold collections of shapes and lights into initialization of scene

--- src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
+++ src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
@@ -1,10 +1,18 @@
 package nl.sogyo.javaopdrachten;
 
+import java.util.ArrayList;
+
 public class Raytracer {
     public static void main(String[] args) {
         Vector viewpoint = new Vector(0, 0, 0);
         Viewport viewport = new Viewport(new Vector(400, 300, 50), new Vector(-400, 300, 50), new Vector(400, -300, 50), 400, 300);
-        Sphere sphereA = new Sphere(new Vector(0, 0, 100), 100);
-        Sphere sphereB = new Sphere(new Vector(100, 150, 130), 50);
+        
+        ArrayList<Shape> shapeList = new ArrayList<Shape>();
+        shapeList.add(new Sphere(new Vector(0, 0, 100), 100));
+        shapeList.add(new Sphere(new Vector(100, 150, 130), 50));
+        
+        ArrayList<LightSource> lightList = new ArrayList<LightSource>();
+        lightList.add(new LightSource(new Vector(500, 500, 155), 100));
+        lightList.add(new LightSource(new Vector(500, -100, 75), 50));
     }
 }
