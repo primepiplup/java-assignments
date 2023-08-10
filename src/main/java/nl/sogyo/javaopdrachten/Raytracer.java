8f331b170f1cd6f6b362220a8c1a9fbfc483da07
Daan Kuik <dkuik@sogyo.nl>
2023-08-10 11:30:20 +0200

    Minor changes in scene layout

--- src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
+++ src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
@@ -10,12 +10,13 @@ public class Raytracer {
         Viewport viewport = new Viewport(new Vector(-36, 27, 50), new Vector(-36, -27, 50), new Vector(36, 27, 50), width, height);
         
         ArrayList<Shape> shapeList = new ArrayList<Shape>();
-        shapeList.add(new Sphere(new Vector(0, 0, 150), 50));
-        shapeList.add(new Sphere(new Vector(20, -20, 100), 10));
+        shapeList.add(new Sphere(new Vector(0, 0, 150), 50, 0.5));
+        shapeList.add(new Sphere(new Vector(20, -20, 100), 10, 0.8));
         
         ArrayList<LightSource> lightList = new ArrayList<LightSource>();
-        lightList.add(new LightSource(new Vector(100, 0, 150), 125));
-        lightList.add(new LightSource(new Vector(0, -60, 120), 50));
+        lightList.add(new LightSource(new Vector(100, 0, 150), 150));
+        lightList.add(new LightSource(new Vector(-50, -60, 80), 100));
+        lightList.add(new LightSource(new Vector(0, 100, 160), 50));
 
         Scene scene = new Scene(viewpoint, viewport, shapeList, lightList);
         scene.render();
