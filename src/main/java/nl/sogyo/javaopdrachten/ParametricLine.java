0f955089cc56add6bcf9ed3fa4ec985f2d7b0a9e
Daan Kuik <dkuik@sogyo.nl>
2023-08-10 09:49:19 +0200

    Added function to check whether point is ahead or behind line

--- src/main/java/nl/sogyo/javaopdrachten/ParametricLine.java
+++ src/main/java/nl/sogyo/javaopdrachten/ParametricLine.java
@@ -9,6 +9,14 @@ public class ParametricLine {
         this.direction = direction;
     }
 
+    public static Boolean isVectorAheadOfLine(Vector vectorToTest, ParametricLine ray) {
+        Line lineToVector = new Line(ray.origin, vectorToTest);
+        ParametricLine rayToVector = lineToVector.getParametricForm();
+        double directionDifferenceAngle = Vector.angle(ray.direction, rayToVector.direction);
+        double distanceFromOrigin = Vector.distance(ray.origin, vectorToTest);
+        return (directionDifferenceAngle < 90 && distanceFromOrigin > 1);
+    }
+
     public void checkForIntersection(ParametricLine intersector) {
         //Not functional for now :(
         Vector a = Vector.crossProduct(direction, intersector.direction);
