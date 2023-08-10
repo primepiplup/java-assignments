5984af0fab7e503ec01003e1dbe74ed2aa641479
Daan Kuik <dkuik@sogyo.nl>
2023-08-10 09:50:07 +0200

    Fixed viewport and changed scene layout for clearer testing

--- src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
+++ src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 public class Raytracer {
     public static void main(String[] args) {
         Vector viewpoint = new Vector(0, 0, 0);
-        int width = 500;
-        int height = 400;
-        Viewport viewport = new Viewport(new Vector(-200, 150, 50), new Vector(-200, -150, 50), new Vector(200, 150, 50), width, height);
+        int width = 600;
+        int height = 450;
+        Viewport viewport = new Viewport(new Vector(-36, 27, 50), new Vector(-36, -27, 50), new Vector(36, 27, 50), width, height);
         
         ArrayList<Shape> shapeList = new ArrayList<Shape>();
-        shapeList.add(new Sphere(new Vector(50, -20, 50), 50));
+        shapeList.add(new Sphere(new Vector(0, 0, 150), 50));
+        shapeList.add(new Sphere(new Vector(20, -20, 100), 10));
         
         ArrayList<LightSource> lightList = new ArrayList<LightSource>();
-        lightList.add(new LightSource(new Vector(500, 500, 155), 100));
-        lightList.add(new LightSource(new Vector(500, -100, 75), 50));
+        lightList.add(new LightSource(new Vector(100, 0, 150), 125));
+        lightList.add(new LightSource(new Vector(0, -60, 120), 50));
 
         Scene scene = new Scene(viewpoint, viewport, shapeList, lightList);
         scene.render();
