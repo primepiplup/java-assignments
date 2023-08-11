26e199a8a02bb75d5365b52d8905090ea3d811a0
Daan Kuik <dkuik@sogyo.nl>
2023-08-11 16:56:58 +0200

    Added reflectivity to material

--- src/main/java/nl/sogyo/javaopdrachten/Material.java
+++ src/main/java/nl/sogyo/javaopdrachten/Material.java
@@ -4,10 +4,12 @@ import java.awt.Color;
 
 public class Material {
     double diffuseCoefficient;
+    double reflectivity;
     Color color;
     
-    public Material(double diffuseCoefficient, Color color) {
+    public Material(double diffuseCoefficient, double reflectivity, Color color) {
         this.diffuseCoefficient = diffuseCoefficient;
+        this.reflectivity = reflectivity;
         this.color = color;
     }
 }
