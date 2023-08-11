62c721d043bc8232e86c9e1dd19d0442eb3752ee
Daan Kuik <dkuik@sogyo.nl>
2023-08-11 14:20:01 +0200

    Added plane with proper parameters to main starting function

--- src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
+++ src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
@@ -3,14 +3,16 @@ package nl.sogyo.javaopdrachten;
 public class Raytracer {
     public static void main(String[] args) {
         Vector viewpoint = new Vector(0, 0, 0);
-        int width = 600;
-        int height = 450;
+        int width = 3840;
+        int height = 2560;
         Viewport viewport = new Viewport(new Vector(-36, 27, 50), new Vector(-36, -27, 50), new Vector(36, 27, 50), width, height);
         
         Scene scene = new Scene(viewpoint, viewport);
 
         scene.addShape(new Sphere(new Vector(0, 0, 150), 50, 0.9));
         scene.addShape(new Sphere(new Vector(20, -20, 100), 10, 0.8));
+        scene.addShape(new Plane(new Vector(0, -75, 200), new Vector(0, 1, 0), 0.5));
+        scene.addShape(new Plane(new Vector(0, 0, 250), new Vector(0, 0, -1), 0.5));
         
         scene.addLight(new LightSource(new Vector(100, 0, 150), 150));
         scene.addLight(new LightSource(new Vector(30, -30, 80), 100));
