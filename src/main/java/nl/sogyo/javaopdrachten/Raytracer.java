61a399c4ca64d96e436ef65ec3beada39e72d68c
Daan Kuik <dkuik@sogyo.nl>
2023-08-11 16:58:11 +0200

    Small changes in scene setup - included reflectivity of materials

--- src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
+++ src/main/java/nl/sogyo/javaopdrachten/Raytracer.java
@@ -11,14 +11,14 @@ public class Raytracer {
         
         Scene scene = new Scene(viewpoint, viewport);
 
-        scene.addShape(new Sphere(new Vector(0, 0, 150), 50, new Material(0.9, Color.BLUE)));
-        scene.addShape(new Sphere(new Vector(20, -20, 100), 10, new Material(0.8, Color.RED)));
-        scene.addShape(new Plane(new Vector(0, -75, 200), new Vector(0, 1, 0), new Material(0.9, Color.WHITE)));
-        scene.addShape(new Plane(new Vector(0, 0, 250), new Vector(0, 0, -1), new Material(0.6, Color.WHITE)));
+        scene.addShape(new Sphere(new Vector(-25, 0, 150), 50, new Material(0.9, 0.0, Color.BLUE)));
+        scene.addShape(new Sphere(new Vector(40, -30, 110), 20, new Material(0.8, 0.0, Color.RED)));
+        scene.addShape(new Plane(new Vector(0, -60, 200), new Vector(0, 1, 0), new Material(0.9, 0.0, Color.WHITE)));
+        scene.addShape(new Plane(new Vector(-150, 0, 300), new Vector(0, 0, -1), new Material(1, 0.0, Color.WHITE)));
         
-        scene.addLight(new LightSource(new Vector(100, 0, 150), 150));
-        scene.addLight(new LightSource(new Vector(30, -30, 80), 100));
-        scene.addLight(new LightSource(new Vector(-100, 100, 160), 75));
+        //scene.addLight(new LightSource(new Vector(100, 0, 150), 150));
+        scene.addLight(new LightSource(new Vector(30, -30, 80), 150));
+        //scene.addLight(new LightSource(new Vector(-100, 100, 160), 75));
 
         SceneRenderer renderer = new SceneRenderer(scene);
 
